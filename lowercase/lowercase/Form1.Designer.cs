8002bbf25622d9da6959cc4bb1d647d0bfd76dd7
fraktal-x <theosg94@gmail.com>
2021-07-20 09:44:16 +0100

    Initial prototype for the text editor. Added basic file functionality.

---
+++ lowercase/lowercase/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace lowercase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rtb
+            // 
+            this.rtb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtb.Location = new System.Drawing.Point(0, 24);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(800, 426);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            this.rtb.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.newToolStripMenuItem.Text = "New...";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "";
+            this.openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rtb);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Lowercase";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ lowercase/lowercase/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lowercase
+{
+    public partial class Form1 : Form
+    {
+        bool saved = true;                 //is the file saved?
+        string currentFile = "";            //curent file name
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e) => ChangesMade();
+        private void newToolStripMenuItem_Click(object sender, EventArgs e) => CreateNewFile();
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) => OpenFile();
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e) => SaveFile();
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e) => SaveFileAs();
+        
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //if not saved, show warning message, and then close application
+            if (!saved)
+                if (DialogResult.OK == MessageBox.Show("You have unsaved changes. All unsaved files will be lost", "Continue?", MessageBoxButtons.OKCancel))
+                    Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //if not saved, show warning message, and then cancel the warning
+            if (!saved)
+                if (DialogResult.Cancel == MessageBox.Show("You have unsaved changes. All unsaved files will be lost", "Continue?", MessageBoxButtons.OKCancel))
+                    e.Cancel = true;
+        }
+
+        /// <summary>
+        /// sets title of WinForm
+        /// </summary>
+        /// <param name="x">the current file name</param>
+        void SetTitle(string x) => this.Text = x + " - lowercase";
+
+        /// <summary>
+        /// called when RTB is updated
+        /// </summary>
+        void ChangesMade() => saved = false;
+
+        /// <summary>
+        /// creates a new file
+        /// </summary>
+        void CreateNewFile()
+        {
+            //if the textbox is not empty...
+            if (rtb.Text != "")
+            {
+                if (DialogResult.OK == MessageBox.Show("You have unsaved changes. All unsaved files will be lost", "Continue?", MessageBoxButtons.OKCancel))
+                {
+                    rtb.Text = "";
+                    currentFile = "";
+                }
+            }
+        }
+
+        /// <summary>
+        /// opens a file using WinForms
+        /// </summary>
+        void OpenFile()
+        {
+            if (!saved)
+                if (DialogResult.OK != MessageBox.Show("You have unsaved changes. All unsaved files will be lost", "Continue?", MessageBoxButtons.OKCancel))
+                    return;
+
+            //attempt to open file
+            try
+            {
+                if (DialogResult.OK == openFileDialog.ShowDialog())
+                {
+                    currentFile = openFileDialog.FileName;
+                    switch (Path.GetExtension(currentFile))
+                    {
+                        case ".txt":
+                            rtb.LoadFile(currentFile, RichTextBoxStreamType.PlainText);
+                            break;
+                        default:
+                            rtb.LoadFile(currentFile);
+                            break;
+                    }
+
+                    SetTitle(currentFile);
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+
+        /// <summary>
+        /// saves the current file
+        /// </summary>
+        void SaveFile()
+        {
+            if (currentFile == "")
+                SaveFileAs();
+            else
+            {
+                rtb.SaveFile(currentFile, RichTextBoxStreamType.PlainText);
+                saved = true;
+            }
+                
+        }
+
+        /// <summary>
+        /// saves the current file as
+        /// </summary>
+        void SaveFileAs()
+        {
+            if (currentFile == "")
+                saveFileDialog.FileName = "New File";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (Path.GetExtension(saveFileDialog.FileName) == ".txt")
+                    rtb.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.PlainText);
+                else
+                    rtb.SaveFile(saveFileDialog.FileName, RichTextBoxStreamType.RichText);
+                currentFile = saveFileDialog.FileName;
+                SetTitle(currentFile);
+                saved = true;
+            }
+           
+        }
+
+    }
+}
